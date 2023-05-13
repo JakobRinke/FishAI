c280f0d7bc1b522c76b053219f18e88207a66b0e
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2023-05-13 09:56:01 +0200

    d

--- src/logic.rs
+++ src/logic.rs
@@ -4,12 +4,12 @@ use socha_client_2023::{client::GameClientDelegate, game::{Move, Team, State}, m
 use std::{f32::INFINITY};
 use socha_client_2023::scoring_funcs::*;
 
+
 /// An empty game logic structure that implements the client delegate trait
 /// and thus is responsible e.g. for picking a move when requested.
 pub struct OwnLogic;
 
 impl GameClientDelegate for OwnLogic {
-
 
     fn request_move(&mut self, state: &State, _my_team: Team) -> Move {
         
@@ -24,12 +24,33 @@ impl GameClientDelegate for OwnLogic {
         //let a: &[f32] = &[ 3.3,  -1.6, 0.78, 0.94, 7.3, 0.3, 2.6, -0.44, 1.6, 0.16 ];
 
         // 
-        let a: &[f32] = &[ 2.4,  -1.9, 1.3, 0.7, 7.3, 0.2, 2.8, -0.3, 1.8, 0.13 ];
+        //let a: &[f32] = &[ 2.4,  -1.9, 1.3, 0.7, 7.3, 0.2, 2.8, -0.3, 1.8, 0.13 ];
+
+        
+        // 
+        //let a: &[f32] = &[ 9., -0.8, 2.15, 0.7, 6.3, 0.6, 2.8, -0.3, 0.5, 0.7 ];
+
+        
+        // let a: &[f32] = &[ 7., -0.6, 2.6, 0.9, 4.3, 0.8, 3.5, -0.25, 0.7, 0.5 ];
+
+        // let a: &[f32] = &[ 6., -0.7, 2.2, 0.5, 3.1, 0.4, 4.5, -0.19, 0.8, 0.4 ];
+
+
+        // Ice Dancer               Art of the Pengu        f(x)
+        // 0.78                         0.5                 1/x
+        //                                                  4*(1-sigm(x))
+        let a: &[f32] = &[  5., -0.7, 3.6, 0.5, 5.7, 0.4, 4.5, -0.19, 0.9, 0.4  ];
 
         let b = a.to_vec();
         info!("round: {}", state.turn());
         info!("score : {}", evaluate(state, 1, &b));
 
+
+
+        print_eval(state, 1, &b);
+        //test_speed(state);
+        // info!("val1: {}", get_field_levels(state, 1));
+        // info!("val2: {}", get_field_levels_2(state, 1));
         if state.turn() <= 7 {
             //let chosen_move = find_best_start_move( *state);
             let mut s = state.clone();
--- src/scoring_funcs.rs
+++ src/scoring_funcs.rs
@@ -2,27 +2,30 @@ use std::{collections::HashMap, cmp::{max, min}};
 
 use log::info;
 
-use crate::game::{State, Team, self, Vec2, Doubled};
+use std::time::Instant;
 
-pub fn get_move_num(gamestate:&State, my_turn:i32) -> i32 {
-    my_turn * (gamestate.possible_moves().len() as i32 - gamestate.opponent_moves().len() as i32)
+
+use crate::game::{State, Team, Vec2, Doubled};
+
+pub fn get_move_num(gamestate:&State, my_turn:i32) -> f32 {
+    my_turn as f32  * (gamestate.possible_moves().len() as f32 - gamestate.opponent_moves().len() as f32)
 }
 
-pub fn get_fish_dif(gamestate:&State, my_turn:i32) -> i32 {
-    my_turn * ( gamestate.fish(gamestate.current_team()) as i32 - 
-                gamestate.fish(gamestate.current_team().opponent()) as i32)
+pub fn get_fish_dif(gamestate:&State, my_turn:i32) -> f32 {
+    my_turn as f32 * ( gamestate.fish(gamestate.current_team()) as f32 - 
+                gamestate.fish(gamestate.current_team().opponent()) as f32)
 }
 
-pub fn get_turn(gamestate:&State) -> i32 {
-    gamestate.turn() as i32
+pub fn get_turn(gamestate:&State) -> f32 {
+    gamestate.turn() as f32
 }
 
-pub fn get_fish_left(gamestate:&State) -> i32 {
-    return gamestate.get_fish_left() as i32;
+pub fn get_fish_left(gamestate:&State) -> f32 {
+    return gamestate.get_fish_left() as f32;
 }
 
 // Optimize
-pub fn get_moveable_peguins(gamestate:&State, team:Team) -> i32 {
+pub fn get_moveable_peguins(gamestate:&State, team:Team) -> f32 {
     let pingus = gamestate.pieces_of(team);
     let board = gamestate.board();
     let mut d = 0;
@@ -31,99 +34,119 @@ pub fn get_moveable_peguins(gamestate:&State, team:Team) -> i32 {
             d += 1;
         }
     }
-    return d;
+    return d as f32;
 }
 
-pub fn get_pengu(gamestate:&State, my_turn:i32) -> i32 {
-    return my_turn * (get_moveable_peguins(gamestate, gamestate.current_team())
+pub fn get_pengu(gamestate:&State, my_turn:i32) -> f32 {
+    return my_turn as f32 * (get_moveable_peguins(gamestate, gamestate.current_team())
              - get_moveable_peguins(gamestate, gamestate.current_team().opponent()));
 }
 
-
-pub fn get_field_control(gamestate:&State, field:Vec2<Doubled>, done:Vec<Vec2<Doubled>> ) -> Vec<Vec2<Doubled>> {
-    let mut dn = done.clone();
-    for new_field in gamestate.board().possible_moves_from(field) {
-        if !dn.contains(&new_field.to()) {
-            dn.push(new_field.to());
-            dn = get_field_control(gamestate, new_field.to(), dn);
-        }
-    }
-    return dn.to_vec();
-}
-
-pub fn get_controlled_fields_of(gamestate:&State, team:Team) -> i32 {
-    let pingus1 = gamestate.pieces_of(team);
-    let mut count = 0;
-    let mut done = vec![];
-    for position in pingus1 {
-        done = get_field_control(gamestate, position.0, done);
-    }
-
-    for f in done {
-        count+=gamestate.board()[f].fish();
-    }
-    return count as i32;
-}
-
-pub fn get_controlled_fields(gamestate:&State, my_turn:i32) -> f32 {
-    return (my_turn * ( get_controlled_fields_of(gamestate, gamestate.current_team()) -
-                        get_controlled_fields_of(gamestate, gamestate.current_team().opponent())
-                    )
-            ) as f32;
-}
-
+// pub fn dj_activator(d:i32, f:usize) -> f32 {
+//     f as f32 / d as f32
+// }
 
 pub fn dj_activator(d:i32, f:usize) -> f32 {
-    f as f32/ (1. + f32::exp(d as f32)) 
+    4. * f as f32 * (1. - 1. / (1. + 2.71828_f32.powf(-d as f32)) )
 }
 
-pub fn add_field_levels(gamestate:&State, field:&Vec2<Doubled>,hm:&mut HashMap<Vec2<Doubled>, f32>, depth:i32) {
-    for new_field in gamestate.board().possible_moves_from(*field) {
-        let mut val = dj_activator(depth, gamestate.board()[new_field.to()].fish()); 
-        if !hm.contains_key(&new_field.to()) {
-            hm.insert(new_field.to(), val);
-            add_field_levels(gamestate, &new_field.to(), hm, depth+1)
-        } else {
-            if let Some(curval) = hm.get_mut(&new_field.to()) {
-                if *curval < val {
-                    *curval = val;
-                    add_field_levels(gamestate, &new_field.to(), hm, depth+1)
-                }
+pub fn get_field_levels_of(gamestate:&State, team1:Team) -> (f32, f32) {
+    let pingus1 = gamestate.pieces_of(team1);
+    let mut done_fields:Vec<Vec2<Doubled>> = vec![];
+    let mut this_fields:Vec<Vec2<Doubled>> = vec![];
+    for position in pingus1 {
+        for m in gamestate.board().possible_moves_from(position.0) {
+            if (!this_fields.contains(&m.to())) {
+                this_fields.push(m.to());
             }
         }
     }
-}
-
-pub fn get_field_levels_of(gamestate:&State, team1:Team) -> f32{
-    let pingus1 = gamestate.pieces_of(team1);
-    let mut hm = HashMap::new();
-    for position in pingus1 {
-        add_field_levels(gamestate, &position.0, &mut hm, 1);
-    }
+    let mut next_fields:Vec<Vec2<Doubled>>;
+    let mut i = 1;
     let mut count:f32 = 0.;
-    for key in hm.values() {
-        count+=*key;
-    }  
-    return count;
+    let mut fcount:usize = 0;
+    while this_fields.len() > 0 {
+        next_fields = vec![];
+        for field in &this_fields {
+            fcount+=gamestate.board()[*field].fish();
+            count+=dj_activator(i, gamestate.board()[*field].fish()) as f32;
+            done_fields.push(*field);
+        }
+        for field in this_fields {
+            for new_field in gamestate.board().possible_moves_from(field) {
+                if !(next_fields.contains(&new_field.to()) || done_fields.contains(&new_field.to())) {
+                    next_fields.push(new_field.to());
+                }
+            }
+        }
+        this_fields = next_fields;
+        i+=1;
+    }
+
+    return (count, fcount as f32);
 }
 
-pub fn get_field_levels(gamestate:&State, my_turn:i32) -> f32 {
+
+pub fn get_field_levels(gamestate:&State, my_turn:i32) -> (f32, f32) {
+    let (l1, f1) = get_field_levels_of(gamestate, gamestate.current_team());
+    let (l2, f2) = get_field_levels_of(gamestate, gamestate.current_team().opponent());
     return (
-        my_turn as f32 * ( 
-                get_field_levels_of(gamestate, gamestate.current_team()) -
-                get_field_levels_of(gamestate, gamestate.current_team().opponent())
-    ));
+        my_turn as f32 * (l1 - l2),
+        my_turn as f32 * (f1 - f2)
+    );
 }
 
 
 
 pub fn evaluate(gamestate:&State, my_turn:i32, args:&Vec<f32>) -> f32 {
     let lateness = 40.0 / gamestate.turn() as f32;
+    let (levels, count) =  get_field_levels(gamestate, my_turn);
     return  args[0] * lateness.powf(args[1]) * get_fish_dif(gamestate, my_turn) as f32
-        +   args[2] * lateness.powf(args[3]) * get_field_levels(gamestate, my_turn) as f32
+        +   args[2] * lateness.powf(args[3]) * levels
         +   args[4] * lateness.powf(args[5]) * get_pengu(gamestate, my_turn) as f32
-      //  +   args[6] * lateness.powf(args[7]) * get_controlled_fish(gamestate, my_turn) as f32
-        +   args[8] * lateness.powf(args[9]) * get_controlled_fields(gamestate, my_turn) as f32
-        
+        //+   args[6] * lateness.powf(args[7]) * get_controlled_fish(gamestate, my_turn) as f32
+        +   args[8] * lateness.powf(args[9]) * count as f32
         ;
+}
+
+pub fn print_eval(gamestate:&State, my_turn:i32, args:&Vec<f32>) {
+
+    let (levels, count) =  get_field_levels(gamestate, my_turn);
+
+    let lateness = 40.0 / gamestate.turn() as f32;
+    info!("Arg 1: {}", args[0] * lateness.powf(args[1]) * get_fish_dif(gamestate, my_turn) as f32);
+    info!("Arg 2: {}", args[2] * lateness.powf(args[3]) * levels);
+    info!("Arg 3: {}", args[4] * lateness.powf(args[5]) * get_pengu(gamestate, my_turn) as f32);
+        //+   args[6] * lateness.powf(args[7]) * get_controlled_fish(gamestate, my_turn) as f32
+    info!("Arg 5: {}", args[8] * lateness.powf(args[9]) * count);      
+}
+
+pub fn test_speed(gamestate:&State) {
+    test_speed_single(get_fish_dif, gamestate);
+    test_speed_double(get_field_levels, gamestate);
+    test_speed_single(get_pengu, gamestate);
+}
+
+
+
+fn test_speed_single(f:fn(gamestate:&State, my_turn:i32)->f32, gamestate:&State) {
+    let now = Instant::now();
+    {
+        for i in 1..2000 {
+            f(gamestate, 1);
+        }
+    }
+    let elapsed = now.elapsed();
+    info!("Arg Speed: {:.2?}", elapsed);
+}
+
+fn test_speed_double(f:fn(gamestate:&State, my_turn:i32)->(f32, f32), gamestate:&State) {
+    let now = Instant::now();
+    {
+        for i in 1..2000 {
+            f(gamestate, 1);
+        }
+    }
+    let elapsed = now.elapsed();
+    info!("Arg Speed: {:.2?}", elapsed);
 }
